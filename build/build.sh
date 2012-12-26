28429c469c9999c1b929d43ed3a4e89eea210940
Karel Bruneel <karel.bruneel@gmail.com>
2012-12-26 21:01:00 +0100

    sadad

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-jsbuild full.cfg -v -o ../lib -j jsts.js
+jsbuild full.cfg -v -u -o ../lib -j jsts.js
 tar -cvf jsts-0.13.1.tar ../src ../doc ../lib ../examples ../src ../license.txt ../authors.txt ../ChangeLog ../README.md
 rm jsts-0.13.1.tar.gz
 gzip -9 jsts-0.13.1.tar
